2518c3f95252ac3b31d0c39cdc83affc7b0eaed6
Prem Chand Saini <prem.inexture@gmail.com>
2020-05-14 13:17:27 +0530

    Complete the project

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -14,7 +14,7 @@ class ExampleTest extends TestCase
      */
     public function testBasicTest()
     {
-        $response = $this->get('/');
+        $response = $this->get('/students');
 
         $response->assertStatus(200);
     }
